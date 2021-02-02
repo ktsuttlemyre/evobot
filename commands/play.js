dbd9a2e5804dc9d6837e7570d9337e495fa3b962
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-02 17:11:47 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -128,7 +128,7 @@ module.exports = {
 
     try {
       queueConstruct.connection = await channel.join();
-      await queueConstruct.connection.voice.setSelfDeaf(true);
+      //await queueConstruct.connection.voice.setSelfDeaf(true);
       play(queueConstruct.songs[0], message);
     } catch (error) {
       console.error(error);
