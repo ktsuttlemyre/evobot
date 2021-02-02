177cd39462bd1ae0c87a1cd793ac0e31bf1cc1cd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-02 18:11:19 -0500

    Create attention.js

---
+++ commands/attention.js
@@ -0,0 +1,20 @@
+const { canModifyQueue, LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
+module.exports = {
+  name: "attention",
+  aliases: ["att"],
+  description: i18n.__("attention.description"),
+  execute(message, args) {
+    const queue = message.client.queue.get(message.guild.id);
+
+    if (!queue) return message.reply(i18n.__("volume.errorNotQueue")).catch(console.error);
+    if (!canModifyQueue(message.member))
+      return message.reply(i18n.__("volume.errorNotChannel")).catch(console.error);
+
+    queue.attention = !queue.attention;
+    return queue.textChannel.send(i18n.__mf("attention.result", { arg: queue.attention })).catch(console.error);
+  }
+};
